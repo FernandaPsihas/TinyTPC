3058ed6d57d3625d78e03de6c8a6a2a4ec5cc893
FernandaPsihas <psihas@fnal.gov>
2023-08-18 09:27:23 -0500

    panos's scripts which he refused to commit...

--- 3x3ops/README.md
+++ 3x3ops/README.md
@@ -1,20 +1,24 @@
 # 3x3 Operations Scripts
 
-This program is executed the following way: ./Run-3x3tile.sh base_flag pedestal_flag threshold_flag different_pedestal_files_flag start_run_flag different_config_files_flag copy_to_dune_flag [either "true" or "false"]
+This program is executed the following way: source Run-3x3tile.sh base_flag pedestal_flag threshold_flag start_run_flag [either "true" or "false"]
 
 - base_flag indicates to the program if base.py should be executed.
 - pedestal_flag indicates to the program if pedestal_qc.py should be executed.
 - threshold_flag indicates to the program if threshold_qc.py should be executed.
-- different_pedestal_files_flag indicates to the program if the input files for threshold_qc.py will be the most recent ones (this requires that pedestal_flag is true also) or will be provided by the user.
 - start_run_flag indicates to the program if start_run_log_raw.py should be executed.
-- different_config_files_flag indicates to the program if the input directory for start_run_log_raw.py will be the most recent one (this requires that threshold_flag is true also) or will be provided by the user.
-- copy_to_dune_flag indicates to the program if the local_directory (see below) should be copied to dunegpvm.
 
+All flags need to have a value before the program can be executed successfully. A script's execution will repeat until it has been successfully completed.  The program creates the following directory in order to store the output files: /tinytpc/data/DATE/DATE_Hour_Minute (the time is based on when the program began its execution) [this is the local_directory].
 
-All flags need to have a value before the script can be executed successfully.Script execution will repeat until the respective script has been successfully completed.  The program creates the following directory in order to store the output files: /tinytpc/data/DATE/DATE_Hour_Minute (the time is based on when the program began its execution) [this is the local_directory].
+When each of the four (4) scripts is executed, a log file is created for every attempt (successful or not) which contains:
+- In its title: the result (success or failure)
+                the attempt's number
+                the date and time (hours:minutes:seconds) when the specific script began.
+- In the log file: the terminal logs
+                   the duration of the attempt is also stored.
 
-Each of the four (4) scripts, when it is executed, a log file is created for every attempt (successful or not) which contain in its title the result (success or failure), the attempt's number and the date and time (hours:minutes:seconds) when the specific script began. In the log file, besides the terminal logs, the duration of the attempt is also stored.
-All output files of these scripts are stored in the local_directory. More specifically, since start_run_log_raw.py requires a directory as input, the output files of threshold_qc.py are stored in local_directory/configs.
-
+All output files of these scripts are stored in the local_directory. In addition, since start_run_log_raw.py requires a directory as input, the output files of threshold_qc.py are stored in local_directory/configs/.
 
 Regarding the user inputs [they should be provided with their full path], the program checks if they exist locally, if they are in the right format (.json and .h5 for the threshold_qc.py inputs and .json for the start_run_log_raw.py inputs) and the same date as the program's execution. Additionally, for the start_run_log_raw.py inputs checks if a configuration file has been submitted for each chip id.
+These input requests are available for:
+- threshold_qc.py, when pedestal_qc.py wasn't executed during the same run.
+- start_run_log_raw.py, when threshold_qc.py wasn't executed during the same run.
--- 3x3ops/RSyncTinyData.sh
+++ 3x3ops/RSyncTinyData.sh
@@ -1,54 +1,61 @@
 #!/bin/bash
+#
+#    This script copies the provided LOCAL_DIRECTORY to dunegpvm (/dune/data/users/psihas/TinyTPC/Bench). A .LOCK file is created during its execution, so that simultaneous runnings are prevented.
+#
+#    Execution: source RSyncTinyData.sh LOCAL_DIRECTORY
+#               [LOCAL_DIRECTORY should be in the following format: /Users/tinytpc/data/DATE/DATE_Hour_Minute -> the user can also upload /Users/tinytpc/data/DATE/ if they wish.]
+#
+#    The script checks if the directory exists, if it was created the same date as this script's execution and if it contains the "/Users/tinytpc/data/"
+#
+#    Created by Fernanda Psihas Olmedo.
+#    Contributor: Panagiotis (Panos) Englezos
+#
 
-################################################
-#
-#     Rsync TinyTPC data for into /dune/data
-#
-#
-#    psihas@fnal.gov
-#
-#
-
-
-
-args=("$@")
-argssize=${#args[*]}
-if [ $argssize -ne 2 ];then
-    echo ""
-    echo "Usage:   ./RSyncTinyData.sh {detector: 3x3, 1chip} {release: ex: S15-11-06}"
-    echo ""
+if [ "$#" -ne 1 ]; then
+    echo "ERROR! Usage: source RSyncTinyData.sh LOCAL_DIRECTORY {/Users/tinytpc/data/DATE/DATE_Hour_Minute}"
     exit
 fi
-DET=${args[0]}
-REL=${args[1]}
 
-LOCKFILE=Rsync_TinyTPV_Files_${DET}.LOCK
+log_directory=$1
+DATE_START_SCRIPT=$(date '+%Y_%m_%d')
 
-if [ $DET == "3x3" ];then
-   # Test to see if this job is already running.
-   if [ -e /tmp/${LOCKFILE} ];then
-      echo "lock file /tmp/${LOCKFILE} exists.  Exiting..."
-      exit
-   fi
-      
-   touch /tmp/${LOCKFILE}
-
-   /usr/krb5/bin/kinit -k -t /home///psihas.cron.dunegpvm01.fnal.gov.keytab psihas/cron/dunegpvm01.fnal.gov@FNAL.GOV
-   
-   /usr/bin/rsync -vura -e "ssh -o StrictHostKeyChecking=no" /Users/tinytpc/data psihas@dune04.fnal.gov:/dune/data/users/psihas/TinyTPC/Bench --exclude '*.LOCK' --exclude '*.DONE' --exclude '*.log'
-   
-   rm -v /tmp/${LOCKFILE}
+if [ ! -d "$log_directory" ];then
+   echo "ERROR! ${log_directory} does not exist."
    exit
-   
-   
+fi
+if [[ $log_directory != *"${DATE_START_SCRIPT}"* ]];then
+   echo "ERROR! ${log_directory} is out of date. Today's date is ${DATE_START_SCRIPT}." # In order to avoid procrastination ;)
+   exit
+else
+   if [[ $log_directory != *"/Users/tinytpc/data/"* ]];then
+     echo "ERROR! ${log_directory} is NOT in /Users/tinytpc/data/" # Since output files are stored in /Users/tinytpc/data/.
+     exit
+   fi
 fi
 
+LOCKFILE=Rsync_TinyTPC_Files_${log_directory}.LOCK
 
+# Test to see if this job is already running.
+if [ -e /tmp/${LOCKFILE} ];then
+    # [ -e FILE ] is "true" if FILE exists.
+    echo "lock file /tmp/${LOCKFILE} exists.  Exiting..."
+    exit
+fi
 
-###### for cronfile.cron # NearDet - RSync to bluearc
-# */20 * * * * /home/novanearline/Nearline-test-releases/S18-01-05/Commissioning/Nearline/RsyncNearline.sh NearDet S18-01-05 > /logs/Nearline/Rsync/Rsync_$(date +\%Y-\%m-\%d_\%T).log 2>&1       
-       #
+touch /tmp/${LOCKFILE}
+# /tmp/${LOCKFILE} is created. The file created using "touch" is empty. This command can be used when the user doesn’t have data to store at the time of file creation.
 
+/usr/krb5/bin/kinit -k -t /Users/tinytpc/Code/psihas.cron.dunegpvm01.fnal.gov.keytab psihas/cron/dunegpvm01.fnal.gov@FNAL.GOV
+# the "-k" option is used because it requests a ticket, obtained from a key in the local host’s keytab. The location of the keytab may be specified with the "-t" keytab_file option
+# "kcroninit" creates the cron principal (<user>/cron/<host>.<domain>@FNAL.GOV) and the keytab file (/path/to/keytab/username.cron.hostname.fnal.gov.keytab).
 
+/usr/bin/rsync -vura -e "ssh -o StrictHostKeyChecking=no" ${log_directory} psihas@dune04.fnal.gov:/dune/data/users/psihas/TinyTPC/Bench --exclude '*.LOCK' --exclude '*.DONE'
+# rsync's  primary advantage over scp is for fast synchronization by only copying new or updated files.
+# Options: -v, --verbose  -> increase verbosity
+#          -a, --archive -> archive mode; same as -rlptgoD (no -H)
+#          -r, --recursive -> recurse into directories
+#          -u, --update -> skip files that are newer on the receiver
+#          -e, --rsh=COMMAND -> specify the remote shell to use
 
-
+rm -v /tmp/${LOCKFILE}
+exit
